eceef4ac0643197cbc007d5c1674bb2e9b6bb728
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-27 14:25:52 -0600

    test: add test for the increase_times_checked method in book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -32,4 +32,17 @@ RSpec.describe Book do
       expect(book.publication_year).to eq("1960")
     end
   end
+
+  describe "#increase_times_checked" do
+    it "returns the amount of times the book has been checked" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+      expect(book.times_checked).to eq(0)
+
+      book.increase_times_checked
+      book.increase_times_checked
+
+      expect(book.times_checked).to eq(2)
+    end
+  end
 end
