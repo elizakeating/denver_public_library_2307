abee707f5edc0872414e648209d2bbeafaa8b739
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-27 14:25:30 -0600

    feat: add times_checked attribute and increase_times_checked method in book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,17 @@
 class Book
   attr_reader :title,
               :author,
-              :publication_year
+              :publication_year,
+              :times_checked
 
   def initialize(data)
     @title = data[:title]
     @author = "#{data[:author_first_name]} #{data[:author_last_name]}"
     @publication_year = data[:publication_date].split(" ").last
+    @times_checked = 0
+  end
+
+  def increase_times_checked
+    @times_checked += 1
   end
 end
