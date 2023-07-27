4b8a2a101755c97baa81fa31e251c0c49b42446c
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-27 14:06:48 -0600

    refactor: change publication_year attribute in book class

--- lib/book.rb
+++ lib/book.rb
@@ -6,6 +6,6 @@ class Book
   def initialize(data)
     @title = data[:title]
     @author = "#{data[:author_first_name]} #{data[:author_last_name]}"
-    @publication_year = data[:publication_date].split(" ")[2]
+    @publication_year = data[:publication_date].split(" ").last
   end
 end
