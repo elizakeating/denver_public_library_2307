208b7f5cba400938a260fc121f8df7b7cb01cde7
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-27 14:30:31 -0600

    feat: add checkout, return, and most_popular_book methods and check_out_books attribute

--- lib/library.rb
+++ lib/library.rb
@@ -1,12 +1,14 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
               
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -23,5 +25,23 @@ class Library
     end
     sorted_dates = dates.sort
     time_frame = {:start => sorted_dates.first.to_s, :end => sorted_dates.last.to_s}
+  end
+
+  def checkout(book)
+    @checked_out_books << book
+    book.increase_times_checked
+  end
+
+  def return(book)
+    book_index = @checked_out_books.index(book)
+    @checked_out_books.delete_at(book_index)
+  end
+
+  def most_popular_book
+    ranking = books.sort_by do |book|
+      book.times_checked
+    end
+
+    ranking.last
   end
 end
